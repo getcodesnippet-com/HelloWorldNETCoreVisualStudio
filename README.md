7b5d87343011d55d181a669c049e13a652a94eda
getcodesnippet-com <info@getcodesnippet.com>
2017-02-09 11:05:49 +0500

    adding project

---
+++ README.md
@@ -0,0 +1,4 @@
+# .NET Core HelloWorld with Visual Studio
+.NET Core HelloWorld program built using Visual Studio 2015
+
+Check out more on [.NET Core](http://getcodesnippet.com/).
---
+++ src/HelloWorldCorewithVisualStudio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldCorewithVisualStudio
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("HelloWord .NET Core with Visual Studio");
+            Console.ReadLine(); 
+        }
+    }
+}
